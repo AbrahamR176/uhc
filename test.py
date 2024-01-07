4b5f0484e0a10dc123406fa2c567f8c9d9d846d7
abrahamr176 <abraham.rincon@mavs.uta.edu>
2024-01-07 13:34:00 -0600

    updated vesion

--- test.py
+++ test.py
@@ -3,15 +3,15 @@ import cv2
 import time
 import numpy as np
 from numpy import array
-import easyocr
 
 pt.pytesseract.tesseract_cmd = 'C:\\Users\\abraham\\AppData\\Local\\Tesseract-OCR\\tesseract.exe'
 
 def main():
     path = "C:\\Users\\abraham\\Documents\\Code\\uhc\\vid2.mp4"
-    process_vid(path)
+    target_text = ["ElRichMC","zadyOne","Kaumaru","tonacho","chincheto77","EvilAFM","Gona","killercreeper","AVISO"]
+    process_vid(path, target_text)
 
-def process_vid(vid_path, frame_target=30):
+def process_vid(vid_path, target_text, frame_target=120):
     #start timer
     start = time.time()
 
@@ -19,8 +19,6 @@ def process_vid(vid_path, frame_target=30):
     im = cv2.VideoCapture(vid_path)
     frames = im.get(cv2.CAP_PROP_FRAME_COUNT)
     success,image = im.read()
-    reader = easyocr.Reader(gpu=False,lang_list=['es','en'], verbose=False)
-
     #counter
     counter = 1
     total = 1
@@ -32,9 +30,11 @@ def process_vid(vid_path, frame_target=30):
         #for every "frame_target" #, do work on the frame
         if counter == frame_target:
             frame = prep_frame(image, total)
-            msg = reader.readtext(image=frame)
+            msg = pt.image_to_string(frame, lang="mc")
             if msg != "":
-                print(f"Frame: {total} of {frames}, text: {msg}")
+                for x in target_text:
+                    if x in msg:
+                        print(f"Frame: {total} of {frames}, text: {msg}")
             counter = 1
         else:
             pass
@@ -49,10 +49,18 @@ def process_vid(vid_path, frame_target=30):
 
 def prep_frame(frame: any, number):
 
-    result = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    result = result[630:680,0:460]
-    result = cv2.resize(result, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
-    result = cv2.inRange(result, array([0, 0, 135]), array([255, 255, 255]))
+    # 1.0
+    #result = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #result = result[630:680,0:460]
+    #result = cv2.resize(result, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+    #result = cv2.inRange(result, array([0, 0, 135]), array([255, 255, 255]))
+
+    #1.1
+    result = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = result[646:664,13:460]
+    result = cv2.resize(result, None, fx=3, fy=3)
+    sharpen_filter = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+    result = cv2.filter2D(result, -1, sharpen_filter)
 
     cv2.imwrite(f"result\\{number}.png",result)
     return result
